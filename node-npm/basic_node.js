d48155a77690d2fa941529ef5931bb19bba96231
Oriel Chaverra <oriel05chaverra@gmail.com>
2021-06-13 21:42:23 -0500

    subida al repo de curso nivelador

---
+++ node-npm/basic_node.js
@@ -0,0 +1,13 @@
+  
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 8080;
+// const _ = require('lodash')
+const server = http.createServer((req, res) => {
+                                 res.statusCode = 200;
+                                 res.setHeader('Content-Type', 'text/html');
+                                 res.end('HOLA ME LLAMO ORIEL CHAVERRA!\n');
+                                 });
+server.listen(port, hostname, () => {
+                        console.log(`Server running at http://${hostname}:${port}/`);
+                      });
